61c6ce7ce0416ee570e7198a66ba82588b4284da
gay <113976105+Fimav2@users.noreply.github.com>
2023-02-05 20:55:15 +0300

    Update easy_list.cpp

--- easy_list.cpp
+++ easy_list.cpp
@@ -17,7 +17,21 @@ void itc_even_index_list (const vector <int> &mass, vector <int> &mass2){
 void itc_even_parts_list(const vector <int> &mass, vector <int> &mass2){
     for(int i = 0; i < mass.size(); i ++)
 
-        if(mass[i]%2==0)
-            mass2.push_back(mass[i]);
+        if(mass[i]%2 == 0){
 
+            mass2.push_back(mass[i]);
+        }
 }
+
+ int itc_positive_list(const vector <int> &mass){
+
+    int chechik = 0;
+    for(int i = 0; i < mass.size();i++){
+        if(mass[i] >= 0)
+            chechik ++;
+
+    }return chechik;
+
+ }
+
+ 
